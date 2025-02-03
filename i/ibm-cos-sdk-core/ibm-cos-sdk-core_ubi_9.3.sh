6c981dd7d2d3d9f3550c57f2483845b60468fc3c
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-02-03 20:18:19 +0530

    Update ibm-cos-sdk-core_ubi_9.3.sh

--- i/ibm-cos-sdk-core/ibm-cos-sdk-core_ubi_9.3.sh
+++ i/ibm-cos-sdk-core/ibm-cos-sdk-core_ubi_9.3.sh
@@ -8,7 +8,7 @@
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : ICH <ich@us.ibm.com>
+# Maintainer    : Ramnath Nayak <Ramnath.Nayak@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
