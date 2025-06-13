093ee9d58627949453a2e92f499892ab3ab9177b
ramnathnayak-ibm <ramnath.nayak@ibm.com>
2025-06-13 17:38:24 +0530

    sphinx-gallery

--- s/sphinx-gallery/sphinx-gallery_ubi_9.3.sh
+++ s/sphinx-gallery/sphinx-gallery_ubi_9.3.sh
@@ -25,7 +25,7 @@ PACKAGE_DIR=sphinx-gallery
 
 CURRENT_DIR=${PWD}
 
-yum install -y git make cmake zip tar wget python3 python3-devel python3-pip gcc-toolset-13 gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc zlib-devel libjpeg-devel 
+yum install -y git make cmake zip tar wget python3 python3-devel python3-pip gcc-toolset-13 gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc zlib-devel libjpeg-devel libxml2-devel libxslt-devel
 
 export GCC_TOOLSET_PATH=/opt/rh/gcc-toolset-13/root/usr
 export PATH=$GCC_TOOLSET_PATH/bin:$PATH
