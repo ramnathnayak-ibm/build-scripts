95916d89d973085555c19de6506e1e1c2befc581
Ramnath Nayak <ramnath.nayak@ibm.com>
2025-11-05 16:24:35 +0530

    Update cChardet_ubi_9.6.sh

--- c/cChardet/cChardet_ubi_9.6.sh
+++ c/cChardet/cChardet_ubi_9.6.sh
@@ -4,7 +4,7 @@
 # Package          : cChardet
 # Version          : master
 # Source repo      : https://github.com/PyYoshi/cChardet
-# Tested on        : UBI:9.3
+# Tested on        : UBI:9.6
 # Language         : Python
 # Travis-Check     : True
 # Script License   : Apache License, Version 2 or later
